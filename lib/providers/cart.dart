d0e11b541131e1c4b8b69bf54fc72efe4e86aeec
saobang001111@gmail.com <you@example.com>
2021-11-05 07:35:08 +0700

    update: snackbar

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -59,6 +59,24 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId]!.quantity > 1) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              title: existingCartItem.title,
+              price: existingCartItem.price,
+              id: existingCartItem.id,
+              quantity: existingCartItem.quantity - 1));
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
   void clear() {
     _items = {};
     notifyListeners();
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -46,6 +46,19 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).accentColor,
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text(
+                  "Added Item to cart!",
+                  textAlign: TextAlign.center,
+                ),
+                duration: const Duration(seconds: 2),
+                action: SnackBarAction(
+                    label: 'Undo',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    }),
+              ));
             },
             icon: const Icon(Icons.shopping_cart),
           ),
